4d997df6e095f5d1f92550d9f6f25fa9563c1b38
chiaohu <67964820+chiaohu@users.noreply.github.com>
2024-09-04 11:29:23 +0800

    Update keyboardPlugin.swift

--- src/ios/keyboardPlugin.swift
+++ src/ios/keyboardPlugin.swift
@@ -4,69 +4,82 @@ import UIKit
 
 @objc(KeyboardPlugin) class KeyboardPlugin: CDVPlugin {
     
-    // 定義 numTxt 為 UITextField 類型
-    var numTxt: UITextField?
+    // 定義變量來存儲輸入框的引用
+    var currentInputField: UITextField?
 
     @objc(addMinusButtonToKeyboard:)
     func addMinusButtonToKeyboard(command: CDVInvokedUrlCommand) {
-        // 調用你的 Swift 方法來設置鍵盤
-        self.addDoneButtonOnKeyboard() // 假設這是你設置鍵盤的 Swift 方法
+        // 為當前焦點輸入框設置自定義鍵盤
+        addDoneButtonOnKeyboard()
         
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Keyboard setup completed")
         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 
-    @objc(minusButtonAction:)
-    func minusButtonAction(command: CDVInvokedUrlCommand) {
-        // 處理 "-" 按鈕點擊事件的 Swift 代碼
-        if let textField = self.numTxt {
-            let currentText = textField.text ?? ""
-            textField.text = currentText + "-"
-        }
-        
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Minus button pressed")
-        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-    }
-
-    @objc(doneButtonAction:)
-    func doneButtonAction(command: CDVInvokedUrlCommand) {
-        // 處理 "確定" 按鈕點擊事件的 Swift 代碼
-        if let textField = self.numTxt {
-            textField.resignFirstResponder()
-        }
-        
-        print("done.....")
-        
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Done button pressed")
-        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-    }
-
-    // 你的 Swift 方法，包括 addDoneButtonOnKeyboard, minusButtonAction, doneButtonAction
+    // 設置鍵盤上的自定義按鈕
     func addDoneButtonOnKeyboard() {
-        // 創建一個新的 UITextField 並設置為插件的屬性
-        self.numTxt = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-        self.numTxt?.borderStyle = .roundedRect
-        
-        // 添加鍵盤上的自定義按鈕
+        // 創建自定義工具欄
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         doneToolbar.barStyle = UIBarStyle.default
         
         let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "確定", style: UIBarButtonItem.Style.done, target: self, action: #selector(doneButtonAction))
         let minusBtn: UIBarButtonItem = UIBarButtonItem(title: "-", style: UIBarButtonItem.Style.plain, target: self, action: #selector(minusButtonAction))
-
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "確定", style: UIBarButtonItem.Style.done, target: self, action: #selector(doneButtonAction))
+        
         var items = [UIBarButtonItem]()
         items.append(minusBtn) // 添加 "-" 按鈕
         items.append(flexSpace)
         items.append(doneBtn)  // 添加 "確定" 按鈕
-
+        
         doneToolbar.items = items
         doneToolbar.sizeToFit()
 
-        // 將工具欄設置為輸入框的輔助視圖
-        self.numTxt?.inputAccessoryView = doneToolbar
+        // 將工具欄設置為當前輸入框的輔助視圖
+        if let textField = currentInputField {
+            textField.inputAccessoryView = doneToolbar
+        }
+    }
 
-        // 在插件中添加 UITextField，例如在視圖的頂部視圖
-        self.webView?.addSubview(self.numTxt!)
+    @objc func minusButtonAction() {
+        // 確保當前有輸入框處於焦點狀態
+        if let textField = currentInputField {
+            let currentText = textField.text ?? ""
+            textField.text = currentText + "-"
+        }
+    }
+
+    @objc func doneButtonAction() {
+        // 收起鍵盤
+        if let textField = currentInputField {
+            textField.resignFirstResponder()
+        }
+    }
+
+    // 監聽輸入框聚焦事件並設置鍵盤
+    override func pluginInitialize() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: Notification) {
+        // 檢查當前焦點的元素是否為輸入框
+        if let activeField = self.webView?.findFirstResponder() as? UITextField {
+            self.currentInputField = activeField
+            self.addDoneButtonOnKeyboard() // 為當前輸入框添加自定義工具欄
+        }
+    }
+}
+
+// 擴展 WKWebView 來查找當前的第一響應者
+extension UIView {
+    func findFirstResponder() -> UIView? {
+        if self.isFirstResponder {
+            return self
+        }
+        for subView in self.subviews {
+            if let firstResponder = subView.findFirstResponder() {
+                return firstResponder
+            }
+        }
+        return nil
     }
 }
