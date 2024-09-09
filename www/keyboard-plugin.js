11af5fd84359851ffe6acb393a3cafb2076f2b65
chiaohu <67964820+chiaohu@users.noreply.github.com>
2024-09-09 10:47:36 +0800

    Update keyboard-plugin.js

--- www/keyboard-plugin.js
+++ www/keyboard-plugin.js
@@ -1,32 +1,43 @@
 var exec = require('cordova/exec');
 
 var KeyboardPlugin = {
-    /**
-     * 顯示鍵盤並添加 "-" 按鈕。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    addMinusButtonToKeyboard: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'addMinusButtonToKeyboard', []);
-    },
-
-    /**
-     * 點擊 "-" 按鈕的處理方法。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    minusButtonAction: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'minusButtonAction', []);
-    },
-
-    /**
-     * 點擊 "確定" 按鈕的處理方法。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    doneButtonAction: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'doneButtonAction', []);
+    addMinusButton: function (success, error) {
+        exec(success, error, 'KeyboardPlugin', 'addMinusButton', []);
     }
 };
 
 module.exports = KeyboardPlugin;
+
+
+// var exec = require('cordova/exec');
+
+// var KeyboardPlugin = {
+//     /**
+//      * 顯示鍵盤並添加 "-" 按鈕。
+//      * @param {Function} successCallback 成功回調
+//      * @param {Function} errorCallback 失敗回調
+//      */
+//     addMinusButtonToKeyboard: function(successCallback, errorCallback) {
+//         exec(successCallback, errorCallback, 'KeyboardPlugin', 'addMinusButtonToKeyboard', []);
+//     },
+
+//     /**
+//      * 點擊 "-" 按鈕的處理方法。
+//      * @param {Function} successCallback 成功回調
+//      * @param {Function} errorCallback 失敗回調
+//      */
+//     minusButtonAction: function(successCallback, errorCallback) {
+//         exec(successCallback, errorCallback, 'KeyboardPlugin', 'minusButtonAction', []);
+//     },
+
+//     /**
+//      * 點擊 "確定" 按鈕的處理方法。
+//      * @param {Function} successCallback 成功回調
+//      * @param {Function} errorCallback 失敗回調
+//      */
+//     doneButtonAction: function(successCallback, errorCallback) {
+//         exec(successCallback, errorCallback, 'KeyboardPlugin', 'doneButtonAction', []);
+//     }
+// };
+
+// module.exports = KeyboardPlugin;
