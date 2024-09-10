49399916f5f614543fc90bcdcd30e70216aeb142
chiaohu <67964820+chiaohu@users.noreply.github.com>
2024-09-10 09:30:15 +0800

    Update keyboard-plugin.js

--- www/keyboard-plugin.js
+++ www/keyboard-plugin.js
@@ -1,32 +1,9 @@
 var exec = require('cordova/exec');
 
-var KeyboardPlugin = {
-    /**
-     * 顯示鍵盤並添加 "-" 按鈕。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    addMinusButtonToKeyboard: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'addMinusButtonToKeyboard', []);
-    },
-
-    /**
-     * 點擊 "-" 按鈕的處理方法。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    minusButtonAction: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'minusButtonAction', []);
-    },
-
-    /**
-     * 點擊 "確定" 按鈕的處理方法。
-     * @param {Function} successCallback 成功回調
-     * @param {Function} errorCallback 失敗回調
-     */
-    doneButtonAction: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'KeyboardPlugin', 'doneButtonAction', []);
+var IOSKeyboardExtension = {
+    addHyphenKey: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'IOSKeyboardExtension', 'addHyphenKey', []);
     }
 };
 
-module.exports = KeyboardPlugin;
+module.exports = IOSKeyboardExtension;
