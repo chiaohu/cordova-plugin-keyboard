0f083ca18dc6baa336aae43c1f545ba9d2130eb7
chiaohu <67964820+chiaohu@users.noreply.github.com>
2024-09-10 09:48:01 +0800

    Update keyboardPlugin.swift

--- src/ios/keyboardPlugin.swift
+++ src/ios/keyboardPlugin.swift
@@ -1,28 +1,45 @@
 import UIKit
 
-@objc(IOSKeyboardExtension) class IOSKeyboardExtension : CDVPlugin {
-    @objc(addHyphenKey:)
-    func addHyphenKey(command: CDVInvokedUrlCommand) {
-        DispatchQueue.main.async {
-            if let keyboardVC = UIInputViewController.init() as? UIInputViewController {
-                let hyphenButton = UIButton(type: .system)
-                hyphenButton.setTitle("-", for: .normal)
-                hyphenButton.sizeToFit()
-                hyphenButton.addTarget(self, action: #selector(self.insertHyphen), for: .touchUpInside)
-                
-                keyboardVC.inputView?.addSubview(hyphenButton)
-                
-                let result = CDVPluginResult(status: CDVCommandStatus_OK)
-                self.commandDelegate.send(result, callbackId: command.callbackId)
-            } else {
-                let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Failed to add hyphen key")
-                self.commandDelegate.send(result, callbackId: command.callbackId)
+@objc(KeyboardMinus) class KeyboardMinus: CDVPlugin {
+    @objc(addMinusButton:)
+    func addMinusButton(command: CDVInvokedUrlCommand) {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+
+        // Create a minus button
+        let minusButton = UIBarButtonItem(title: "-", style: .plain, target: self, action: #selector(self.minusButtonTapped))
+
+        // Flexible space to push the button to the right
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        // Add the minus button to the toolbar
+        toolbar.setItems([flexibleSpace, minusButton], animated: false)
+
+        // Add the toolbar to the keyboard
+        if let activeTextField = getActiveTextField() {
+            activeTextField.inputAccessoryView = toolbar
+        }
+
+        // Send a success callback to JavaScript
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc func minusButtonTapped() {
+        if let activeTextField = getActiveTextField() {
+            // Insert minus sign to the text field
+            if let text = activeTextField.text {
+                activeTextField.text = text + "-"
             }
         }
     }
-    
-    @objc func insertHyphen() {
-        UIPasteboard.general.string = "-"
-        NotificationCenter.default.post(name: UITextView.textDidChangeNotification, object: nil)
+
+    private func getActiveTextField() -> UITextField? {
+        // Traverse through the view hierarchy to find the active UITextField
+        let keyWindow = UIApplication.shared.connectedScenes
+            .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }
+            .first { $0.isKeyWindow }
+
+        return keyWindow?.findFirstResponder() as? UITextField
     }
 }
